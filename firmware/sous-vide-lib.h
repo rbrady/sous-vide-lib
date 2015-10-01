f31358e5e83853447b7e2cbb320f6f3041c7d5f8
Ryan Brady <ryan@ryanbrady.org>
2015-10-01 18:52:14 -0400

    Update sous-vide-lib.h

--- firmware/sous-vide-lib.h
+++ firmware/sous-vide-lib.h
@@ -1,1 +1,8 @@
-// this is a test
+// This #include statement was automatically added by the Particle IDE.
+#include "relay-lib/relay-lib.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "pid/pid.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "OneWire/OneWire.h"
